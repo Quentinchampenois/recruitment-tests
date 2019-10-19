38f05cd80e8e2082db7b63077e666e9f9eca519e
Quentin Champenois <quentinchampenois@MacBook-Pro-de-Quentin.local>
2019-10-19 16:34:55 +0200

    shell scripts refactoring (mkcd.sh / welcome_bye.sh)

--- shell/mkcd.sh
+++ shell/mkcd.sh
@@ -2,15 +2,17 @@
 
 # mkcd function create folder, and .gitkeep file in then change directory
 mkcd() {
-
+    # find user name
     username=$(whoami)
+    # if the folder already exists, print a message otherwise create new folder and a file within
     if [ ! -d ${username}-folder ]
     then
-        mkdir ${username}-folder
-        touch ./${username}-folder/.gitkeep
-        cd ./${username}-folder/
+        mkdir "${username}-folder"
+        touch "${username}-folder/.gitkeep"
+        # TODO: find a way to change directory in the current shell
+        cd "$PWD/${username}-folder/" || echo "can't change directory"
     else
-        echo "File already existing"
+        echo "Folder exists"
     fi
 
     exit 0
--- shell/welcome_bye.sh
+++ shell/welcome_bye.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-# Verify if $1 is "hello" or "bye" otherwise print a stopping message
+# Verify if $1 is "hello" or "bye" otherwise print a stopping message and exit program
 main() {
-    if [[ $1 -eq "hello" || $1 -eq "bye" ]]; then
-        welcome_bye $1
+    if [[ "$1" -eq "hello" || "$1" -eq "bye" ]]; then
+        welcome_bye "$1"
     else
         echo "No argument provided, script requires argument"
         exit 1
@@ -13,11 +13,11 @@ main() {
 
 # Print byebye or welcome, depends of $1
 welcome_bye() {
-    if [[ $1 == "hello" ]]; then
+    if [[ "$1" == "hello" ]]; then
         echo "Welcome ${USER}"
     else
         echo "Byebye ${USER}"
     fi
 }
 
-main $1
+main "$1"
